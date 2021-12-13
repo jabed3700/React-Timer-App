f98722bb025d7e1ecef3664d42c39132a2802f7f
jabed37 <jabed.aleshasolutions@gmail.com>
2021-12-13 12:52:27 +0600

    add increment && decrement btn with function

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -9,13 +9,20 @@ class App extends React.Component{
         this.setState({count:this.state.count+1})
     }
     decrementCount = () => {
-        this.setState({count:this.state.count-1})
+       if(this.state.count > 0){
+            this.setState({count:this.state.count-1})
+       }
     }
     render() {
         
         return (
              <div className="App">
                  <h1 className="Heading">React Timer App</h1>
+                <div className="Container">
+                    <button className="Btn" onClick={this.decrementCount}> - </button>
+                    <p className="Text">{this.state.count}</p>
+                    <button className="Btn" onClick={this.incrementCount}> + </button>
+                </div>
              </div>
         )
     }
