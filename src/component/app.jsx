0deb737a1262d503f6fa64048915b50a492efcd4
jabed37 <jabed.aleshasolutions@gmail.com>
2021-12-13 12:39:14 +0600

    create increateCount,decreate,state in app class

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import './app.style.css'
 
 class App extends React.Component{
+    state= {
+        count:0,
+    }
+    incrementCount = ()=>{
+        this.setState({count:this.state.count+1})
+    }
+    decrementCount = () => {
+        this.setState({count:this.state.count-1})
+    }
     render() {
+        
         return (
-             <h1>Amar sonar bangla</h1>
+             <div className="App">
+                 <h1 className="heading">Amar sonar bangla</h1>
+             </div>
         )
     }
 }
