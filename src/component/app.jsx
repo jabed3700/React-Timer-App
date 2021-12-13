ab777fc60a5834e42982e9e94bbffe132e0d7c4e
jabed37 <jabed.aleshasolutions@gmail.com>
2021-12-13 12:32:58 +0600

    create react environment

---
+++ src/component/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './app.style.css'
+
+class App extends React.Component{
+    render() {
+        return (
+             <h1>Amar sonar bangla</h1>
+        )
+    }
+}
+export default App;
